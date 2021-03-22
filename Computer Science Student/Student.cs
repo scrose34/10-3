88e10201f087e49c3c691be7a727afb7f329b0ac
Kleeborp <Kleeborp@DESKTOP-05QGDPB>
2021-03-22 19:07:45 -0400

    Add project files.

---
+++ Computer Science Student/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Computer_Science_Student
+{
+    abstract class Student
+    {
+        // Fields
+        private string _name;
+        private string _id;
+        // Constructor
+        public Student(string name, string id)
+        {
+            _name = name;
+            _id = id;
+        }
+
+        //Name property
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        // RequiredHours property (abstract)
+        public abstract double RequiredHours
+        {
+            get;
+        }
+    }
+}
